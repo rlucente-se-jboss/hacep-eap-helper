bcdebe9a5215bb797975cb9ddf1bd38078dfb754
rlucente <rlucente@redhat.com>
2018-08-14 13:09:19 -0400

    minor edit

--- check-eap-status.sh
+++ check-eap-status.sh
@@ -5,7 +5,7 @@ for i in {1..4}
 do
     jboss-eap-7.0/bin/jboss-cli.sh -c \
         --command="/host=master/server-config=hacep-$i:read-attribute(name=status)"
-done | grep 'STARTED' | wc -l | xargs echo -n "Started "
+done | grep 'STARTED' | wc -l | xargs echo -n "Started"
 echo " out of 4 servers."
 echo
 

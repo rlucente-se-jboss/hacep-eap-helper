8000e13ab738adc77d3dfd19e93c081dff1c2501
rlucente <rlucente@redhat.com>
2018-09-27 18:06:54 -0400

    fix default server count

--- check-eap-status.sh
+++ check-eap-status.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo
-for i in {1..4}
+for i in {1..2}
 do
     jboss-eap-7.0/bin/jboss-cli.sh -c \
         --command="/host=master/server-config=hacep-$i:read-attribute(name=status)"
 done | grep 'STARTED' | wc -l | xargs echo -n "Started"
-echo " out of 4 servers."
+echo " out of 2 servers."
 echo
 

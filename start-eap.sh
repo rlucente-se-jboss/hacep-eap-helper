2fd0aaca7fd46fe37b4bb171efd899634f6ee3fb
rlucente <rlucente@redhat.com>
2018-10-02 09:50:22 -0400

    clear log when starting EAP

--- start-eap.sh
+++ start-eap.sh
@@ -3,6 +3,8 @@
 . $(dirname $0)/demo.conf
 
 PUSHD $WORKDIR
+    find ./jboss-eap-${VER_INST_EAP} -type f \
+        -name '*.log' -exec truncate -s 0 {} \;
     ./jboss-eap-${VER_INST_EAP}/bin/domain.sh
 POPD
 
